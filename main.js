39ce9133a86ea232911f3a5c56f5dc0bdc12a738
vichithchandran <vichithchandran@gmail.com>
2024-10-15 12:40:50 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+let items = document.querySelectorAll('.slider .item');
+let prevBtn = document.getElementById('prev');
+let nextBtn = document.getElementById('next');
+let lastPosition = items.length -1;
+let firstPosition = 0;
+let active = 1;
+
+nextBtn.onclick =() =>{
+  active++;
+  setSlider();
+}
+prevBtn.onclick =() =>{
+  active--;
+  setSlider();
+}
+
+const setSlider = () =>{
+  let oldActive = document.querySelector('.slider .list .item.active');
+  if(oldActive) oldActive.classList.remove('active');
+  items[active].classList.add('active');
+
+  nextBtn.classList.remove('d-none');
+  prevBtn.classList.remove('d-none');
+
+ if(active === lastPosition) nextBtn.classList.add('d-none');
+ if(active === firstPosition) prevBtn.classList.add('d-none');
+
+}
+
+const setDiameter = () => {
+  let slider = document.querySelector('.slider');
+  let widthSlider = slider.offsetWidth;
+  let heightSlider = slider.offsetHeight;
+  let diameter = Math.sqrt(Math.pow(widthSlider, 2) + Math.pow(heightSlider, 2));
+  document.documentElement.style.setProperty('--diameter', diameter + 'px');
+};
+
+setDiameter();
+
+window.addEventListener('resize', setDiameter)
